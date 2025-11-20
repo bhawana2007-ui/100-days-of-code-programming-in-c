94fcc9e117541fd558cfa535869a5e289e142258
bhawana2007-ui <bhawana2007.7@gmail.com>
2025-11-21 00:30:49 +0530

    DAY42 Q83,Q84
    Q83 (Strings)
    Count vowels and consonants in a string.
    Q84 (Strings)
    Convert a lowercase string to uppercase without using built-in functions.

---
+++ q83.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int vowels = 0, consonants = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        char ch = str[i];
+
+        // Check for vowels
+        if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' ||
+            ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U') {
+            vowels++;
+        }
+        // Check for consonants (alphabet but not vowel)
+        else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+            consonants++;
+        }
+    }
+
+    printf("Vowels: %d\n", vowels);
+    printf("Consonants: %d\n", consonants);
+
+    return 0;
+}
